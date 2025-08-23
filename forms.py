38c105daad74361dfbc89009ae627aca5200b234
Rafael <carballo.rafael91@users.noreply.github.com>
2025-08-22 21:16:48 -0400

    Initial commit: Flask construction site project

--- forms.py
+++ forms.py
@@ -81,7 +81,8 @@ class ContactForm(FlaskForm):
                 return False
 
             # optional hostname check:
-            expected = url_for('home')
+            expected = 'https://planelectricllc.com/'
+            print(f"Expected {expected}")
             if expected and verify_resp.get("hostname") not in (expected, "localhost", "127.0.0.1"):
                 self.turnstile_token.errors.append("Invalid captcha hostname.")
                 return False
